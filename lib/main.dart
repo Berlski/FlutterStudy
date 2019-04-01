d7a076040038033f1592210e41235734c46146de
Berlsky <ccp@efanghang.com>
2019-04-01 16:07:57 +0800

    自定义主题

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    title: 'Flutter Tutorial',
-    home: new TutorialHome(),
-  ));
+  runApp(MaApp());
 }
 
-class TutorialHome extends StatelessWidget {
+class MaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final appName = '自定义主题';
 
     if (Platform.isAndroid) {
       // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
@@ -21,39 +19,54 @@ class TutorialHome extends StatelessWidget {
       SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
     }
 
-    //Scaffold是Material中主要的布局组件.
-    return new Scaffold(
-      appBar: new AppBar(
-
-          elevation:10,
-
-        leading: new IconButton(
-          icon: new Icon(Icons.menu),
-          tooltip: '导航菜单',
-          onPressed: null,
-        ),
-        title: new Text('范例标题'),
-        actions: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.search),
-            tooltip: '搜索',
-            onPressed: null,
-          ),
-        ],
+    return new MaterialApp(
+      title: appName,
+      theme: new ThemeData(
+        //应用程序整体主题的亮度 ,内容部分的亮度
+        brightness: Brightness.light,
+        //APP主要部分的背景颜色，标题栏的颜色
+        primaryColor: Colors.red,
+        //前景色：文本、按钮等
+        accentColor: Colors.orange,
+        textSelectionColor: Colors.white,
       ),
-
-
-      //body占屏幕的大部分
-      body: new Center(
-        child: new Text('Hello, world!'),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        elevation:30,
-        tooltip: '加加加!', // used by assistive technologies
-        child: new Icon(Icons.add),
-        onPressed: null,
+      home: new MyHome(
+        title: appName,
       ),
     );
   }
 }
 
+class MyHome extends StatelessWidget {
+  final String title;
+
+  const MyHome({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text(title),
+      ),
+      body: new Center(
+        child: new Container(
+          color: Theme.of(context).accentColor,
+          child: new Text(
+            '带有背景颜色的文本组件',
+            style: Theme.of(context).textTheme.title,
+          ),
+        ),
+      ),
+      floatingActionButton: new Theme(
+          data: Theme.of(context).copyWith(accentColor: Colors.grey),
+          child: new FloatingActionButton(
+            onPressed: null,
+            child: new Icon(
+              Icons.computer,
+              color: Colors.white,
+            ),
+          )),
+    );
+  }
+}
