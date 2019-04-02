38c232f9cd593dc4af7b24cc3c704f27540caeac
Berlsky <ccp@efanghang.com>
2019-04-02 13:58:55 +0800

    使用第三方库，开启本地浏览器百度网页

--- build/frontend_server.d
+++ build/frontend_server.d
@@ -1,1 +1,1 @@
-frontend_server.d: C:\Users\ccp\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
+frontend_server.d: C:\Users\ccp\ide\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
-//MyApp 不需要做状态处理 所以此组件继承 StatelessWidget
 class MyApp extends StatelessWidget {
-  //这个组件是整个应用的主组件
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: '标题',
-      home: new MyHomePage(
-        title: 'asfs',
-      ),
-      theme: new ThemeData(
-        accentColor: Colors.red,
-        primaryColor: Colors.red,
-        brightness: Brightness.light,
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  //标题
-  final String title;
-
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  //必须重写 createState 方法
-  @override
-  _MyHomePagerState createState() => new _MyHomePagerState();
-}
-
-//状态类必须继承 State 类，注意后面需要指定为<MyHomePage>
-class _MyHomePagerState extends State<MyHomePage> {
-  int _counter = 0;
-
-  //函数调用 State 中的 setState 方法，来更改状态值，使变量变化
-  void _incrementCounter() {
-    setState(() {
-      //计数器变量，每次被调用数值加一
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(
-          '$_counter',
+      title: '使用第三方库',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('使用第三方库'),
         ),
-      ),
+        body: new Center(
+          child: new RaisedButton(
+            color: Colors.white,
+            onPressed: () {
+              //指定url 并发起请求
+              const url ='https://www.baidu.com';
+              launch(url);
+            },
 
-      //居中布局
-      body: new Center(
-        //垂直布局（满屏）
-        child: new Column(
-          //主轴居中对齐
-          mainAxisAlignment: MainAxisAlignment.center,
-
-          //子组件数组
-          children: <Widget>[
-            new Text(
-              '这是一个神奇的按钮' + '$_counter',
-              style: Theme.of(context).textTheme.body1,
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.body2,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'icon',
-        child: new Icon(
-          Icons.add,
-          color: Colors.white,
+            child: new Text('打开百度'),
+          ),
         ),
       ),
     );
